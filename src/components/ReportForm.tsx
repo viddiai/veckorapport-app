0a08b509af7e618d212d2d8ad54525d82ab26ecc
viddiai <stefan@peakmarketing.io>
2025-11-18 13:25:31 +0100

    Fix: Remove unused useEffect import

--- src/components/ReportForm.tsx
+++ src/components/ReportForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import type { WeeklyReport, Project, ReportFormData } from "../types";
 import { generateId, getWeekDates } from '../utils/dateHelpers';
 import { Button } from './Button';
